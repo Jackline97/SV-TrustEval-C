8b978b56854dbbdd3b2b818ddc3ffbd55ff799f1
Jackline97 <43145826+Jackline97@users.noreply.github.com>
2025-09-15 16:00:12 -0400

    Update Run_Eval_script.py

--- Eval_Script/Run_Eval_script.py
+++ Eval_Script/Run_Eval_script.py
@@ -2,8 +2,9 @@ import subprocess
 
 command = [
     "python", "Eval_script.py",
-    "--root_folder", "SV-TrustEval-C-Offical-1.0_results/LLM_result_zero-shot_0.0",
-    "--save_path", "SV-TrustEval-C-Offical-1.0_results/eval_score",
+    "--root_folder", "SV-TrustEval-C-Official-1.0_results/LLM_result_zero-shot_0.0",
+    "--save_path", "SV-TrustEval-C-Official-1.0_results/eval_score",
 ]
 # Execute the command and check for errors
+
 subprocess.run(command, check=True)
